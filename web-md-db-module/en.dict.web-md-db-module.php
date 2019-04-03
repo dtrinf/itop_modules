6fae0e597011da9c6797a5501ad649849215fc7b
David Trigo Chávez <david.trigo@gmail.com>
2019-04-03 15:24:38 +0200

    New Module
    New module to link items
    WebApplication --> MiddlewareInstance
    WebApplication --> DatabaseInstance
    MiddlewareInstance -- DatabaseInstance

---
+++ web-md-db-module/en.dict.web-md-db-module.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:DatabaseSchema/Attribute:middlewares_list' => 'MiddlewareInstance Applications',
+	'Class:DatabaseSchema/Attribute:middlewares_list+' => '',
+	'Class:MiddlewareInstance/Attribute:dbinstances_list' => 'Database Instances',
+	'Class:MiddlewareInstance/Attribute:dbinstances_list+' => '',
+	'Class:DatabaseSchema/Attribute:webapplications_list' => 'Web Applications',
+	'Class:DatabaseSchema/Attribute:webapplications_list+' => '',
+	'Class:WebApplication/Attribute:dbinstances_list' => 'Database Instances',
+	'Class:WebApplication/Attribute:dbinstances_list+' => '',
+	'Class:MiddlewareInstance/Attribute:webapplications_list' => 'Web Applications',
+	'Class:MiddlewareInstance/Attribute:webapplications_list+' => '',
+	'Class:WebApplication/Attribute:middlewareinstances_list' => 'Middleware Instances',
+	'Class:WebApplication/Attribute:middlewareinstances_list+' => '',
+));
+?>
