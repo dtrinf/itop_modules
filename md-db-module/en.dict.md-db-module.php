1456b3006a480891ef8e17c027c0f0f0c7a63711
David Trigo Chávez <david.trigo@gmail.com>
2019-04-03 12:40:47 +0200

    New Modules

---
+++ md-db-module/en.dict.md-db-module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:DatabaseSchema/Attribute:middlewares_list' => 'MiddlewareInstance Applications',
+	'Class:DatabaseSchema/Attribute:middlewares_list+' => '',
+	'Class:MiddlewareInstance/Attribute:dbinstances_list' => 'Database Instances',
+	'Class:MiddlewareInstance/Attribute:dbinstances_list+' => '',
+));
+?>
---
+++ web-md-module/en.dict.web-md-module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:MiddlewareInstance/Attribute:webapplications_list' => 'Web Applications',
+	'Class:MiddlewareInstance/Attribute:webapplications_list+' => '',
+	'Class:WebApplication/Attribute:middlewareinstances_list' => 'Middleware Instances',
+	'Class:WebApplication/Attribute:middlewareinstances_list+' => '',
+));
+?>
